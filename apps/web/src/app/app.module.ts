314885496b10053d412cb1d8a4a39dc09fd04761
Felipe Butzke <felip.ab@gmail.com>
2021-11-09 21:57:03 -0300

    adicionado navbar e estilos css

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
