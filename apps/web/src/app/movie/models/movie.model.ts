1350c2902e244bb2700532b80e51a80d8e600bd2
Felipe Butzke <felip.ab@gmail.com>
2021-11-10 07:44:27 -0300

    criado movie model e movie service

---
+++ apps/web/src/app/movie/models/movie.model.ts
@@ -0,0 +1,73 @@
+export interface Movie {
+  id: number;
+  nome: string;
+  genero: string;
+  imagem: string;
+  ano: number;
+}
+
+export const movies: Movie[] = [
+  {
+    id: 1,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 2,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 3,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 4,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 5,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 6,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+  {
+    id: 7,
+    nome: 'Stowaway',
+    genero: 'Sci-fi',
+    imagem:
+      'https://img.yts.mx/assets/images/movies/stowaway_2021/medium-cover.jpg',
+    ano: '2021',
+  },
+
+];
